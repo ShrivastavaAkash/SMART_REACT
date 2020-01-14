122e0a6c715579937160ddd8089c286ffa67f5d7
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2020-01-14 18:17:36 +0530

    Updates

---
+++ precache-manifest.9597094aacd36e42f7c34dc34652ea06.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436c1219ef088c27b2ce7bd8179328a1",
+    "url": "/SMART_REACT/index.html"
+  },
+  {
+    "revision": "a704723ccf24216648e4",
+    "url": "/SMART_REACT/static/js/2.8944be89.chunk.js"
+  },
+  {
+    "revision": "23bdad1c6827252fd600",
+    "url": "/SMART_REACT/static/js/main.b0fca05c.chunk.js"
+  },
+  {
+    "revision": "2f0044b3a4d18bcc49e7",
+    "url": "/SMART_REACT/static/js/runtime~main.8c3d8d06.js"
+  }
+]);
